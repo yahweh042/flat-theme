de578f3b3babb1b7b8426c2adaa41cda9576fd36
xuyi2 <xuyi2@liepin.com>
2022-04-02 13:37:35 +0800

    flat theme

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,18 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2021.3.3")
+    version.set("2020.1")
+    type.set("IC")
+    updateSinceUntilBuild.set(false)
 }
 tasks {
     patchPluginXml {
         changeNotes.set("""
             Add change notes here.<br>
             <em>most HTML tags may be used</em>        """.trimIndent())
+    }
+
+    buildSearchableOptions {
+        enabled = false
     }
 }
